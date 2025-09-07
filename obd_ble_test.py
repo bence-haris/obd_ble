779a49532871f0ab94b4d30f798f6aa0bdeee667
BenHaris <gibson.bence@citromail.com>
2025-09-07 22:24:37 +0200

    Modularise response handling

--- obd_ble_test.py
+++ obd_ble_test.py
@@ -11,17 +11,57 @@ RX_CHAR = "0000fff1-0000-1000-8000-00805f9b34fb"
 commands = {"rpm":"30313043300d", "echo":"415445300d", "device":"4154490d", "speed": "30313044300d", "fuel":"30313545300d"}
 response_data = []
 
+
+def calculate_rpm(high_bit, low_bit):
+    return (high_bit * 256 + low_bit) / 4
+
+
+def calculate_kmh(hex_bit):
+    return int(hex_bit[6:8],16)
+
+
+def decode_response(response_bytes):
+    response_array=response_bytes.decode('ascii', errors='ignore').strip().split('\n')
+    output = str()
+    for data_snippet in response_array:
+
+        try:
+            # Service 01 Response - Show current data
+            if data_snippet[:2] == "41":
+                # PID (in Hex)
+                match data_snippet[3:5]:
+                    # RPM
+                    case "0C":
+                        val_high=int(data_snippet[6:8],16)
+                        val_low=int(data_snippet[9:11],16)
+
+                        output = f"RPM: {calculate_rpm(val_high, val_low)}"
+                        return output
+                    # Speed
+                    case "0D":
+                        val_hex = int(data_snippet[6:8], 16)
+
+                        output = f"Speed: {calculate_kmh(val_hex)}"
+                        return output
+            # Other command given or response is undefined
+            else:
+                output += str(data_snippet)
+        except IndexError:
+            continue
+    return output
+
+
 # Incoming notifications
 # Receive response bytearray
 def notification_handler(sender, data: bytearray):
     response_data.append(data)
-    
+
     # Process response
     if response_data:
         # Combine all notification data
         full_response = b''.join(response_data)
-        ascii_response = full_response.decode('ascii', errors='ignore').strip()
-        print(ascii_response)
+        response_decoded = decode_response(full_response)
+        print(response_decoded)
     else:
         print("No response")
 
@@ -30,18 +70,18 @@ async def send_obd2_command(client, command):
     # Send command as bytes
     await client.write_gatt_char(TX_CHAR, bytes.fromhex(command), response=True)
     # Wait for response
-    await asyncio.sleep(3)
+    await asyncio.sleep(1)
 
-    
+
 async def main():
-    command = commands["device"]
+    command = commands["speed"]
     async with BleakClient(MAC) as client:
         # Enable notifications
         await client.start_notify(RX_CHAR, notification_handler)
-        
+
         # Send command
         await send_obd2_command(client, command)
-        
+
         # Stop notifications
         await client.stop_notify(RX_CHAR)
 
